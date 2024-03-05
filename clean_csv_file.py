07a3604d69d4724030e105cba0cc6f09a9b8ec8d
Tayo Ajilo <adetayoajilo@gmail.com>
2024-03-05 21:35:07 +1300

    Add more csv and improve our python code

--- clean_csv_file.py
+++ clean_csv_file.py
@@ -1,14 +1,43 @@
+# import pandas as pd
+
+# list_of_files = ['NBA_Player_Stats.csv', 'NBA_Playoff_Stats.csv', 'NBA_total_rookies_stats_1980_2023.csv']
+
+# for file in list_of_files:
+#     # Read the CSV file into a DataFrame
+#     df = pd.read_csv(file)
+
+
+#     # Remove '%' symbols from column names
+#     new_columns = [col.replace('%', '_PCT') for col in df.columns]
+#     df.columns = new_columns
+
+
+#     # Save the DataFrame back to a CSV file
+#     df.to_csv(f'./seeds/{file.lower()}', index=False)
+
+
+
 import pandas as pd
+import re
 
+list_of_files = ['NBA_Player_Stats.csv', 'NBA_Playoff_Stats.csv', 'NBA_total_rookies_stats_1980_2023.csv']
 
-# Read the CSV file into a DataFrame
-df = pd.read_csv('NBA_Player_Stats.csv')
+for file in list_of_files:
+    # Read the CSV file into a DataFrame
+    df = pd.read_csv(file)
 
+    # Remove '%' symbols from column names and ensure they start with a letter
+    new_columns = []
+    for col in df.columns:
+        # Remove '%' symbols
+        new_col = col.replace('%', '_pct').lower()
+        # Ensure the column name starts with a letter
+        if re.match('^\d', new_col):
+            new_col = 'c_' + new_col  # Prepend 'C' if it starts with a number
+        new_columns.append(new_col)
 
-# Remove '%' symbols from column names
-new_columns = [col.replace('%', '_PCT') for col in df.columns]
-df.columns = new_columns
+    # Update column names
+    df.columns = new_columns
 
-
-# Save the DataFrame back to a CSV file
-df.to_csv('./seeds/NBA_Player_Stats.csv', index=False)
+    # Save the DataFrame back to a CSV file
+    df.to_csv(f'./seeds/{file.lower()}', index=False)
