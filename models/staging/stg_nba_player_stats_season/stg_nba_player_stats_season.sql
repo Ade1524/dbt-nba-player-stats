cbabb97d3e8f1f05947f7e11a38792678e9a8a34
Adetayo Aruwajoye <adetayoajilo@gmail.com>
2024-03-23 00:30:26 +0000

    editing few queries on the staging models

--- models/staging/stg_nba_player_stats_season/stg_nba_player_stats_season.sql
+++ models/staging/stg_nba_player_stats_season/stg_nba_player_stats_season.sql
@@ -2,40 +2,38 @@ with nba_player_stats as (
     select * from {{ ref('nba_player_stats')}}
 )
 
-
-
 , rename_player_stats_columns as (
       select rk as rank_id
             ,replace(replace(player,'*', ' '), '-', ' ') as player_full_name
-            ,pos as position
-            ,age as age
-            ,tm as team
-            ,g as games
-            ,gs as games_started
-            ,mp as minutes_played_per_game
-            ,fg as field_goals_per_game
-            ,fga as field_goal_attempts_per_game
-            ,fg_pct as field_goal_percentage
-            ,c_3p as c_3_point_field_goals_per_game
-            ,c_3pa as  c_3_point_field_goal_attempts_per_game
-            ,c_3p_pct as c_3_point_field_goal_percentage
-            ,c_2p as c_2_point_field_goals_per_game
-            ,c_2pa as c_2_point_field_goal_attempts_per_game
-            ,c_2p_pct as c_2_point_field_goal_percentage
-            ,efg_pct as effective_field_goal_percentage
-            ,ft as free_throws_per_game
-            ,fta as  free_throw_attempts_per_game
-            ,ft_pct as free_throw_percentage
-            ,orb as  offensive_rebounds_per_game
-            ,drb as defensive_rebounds_per_game
-            ,trb as total_rebounds_per_game
-            ,ast as assists_per_game
-            ,stl as steals_per_game
-            ,blk as blocks_per_game
-            ,tov as turnovers_per_game
-            ,pf as personal_fouls_per_game
-            ,pts as points_per_game
-            ,year as season
+            ,pos::varchar(1000) as position
+            ,age::int as age
+            ,tm::varchar(1000) as team
+            ,g::int as games
+            ,gs::int as games_started
+            ,mp::float as minutes_played_per_game
+            ,fg::float as field_goals_per_game
+            ,fga::float as field_goal_attempts_per_game
+            ,fg_pct::float as field_goal_percentage
+            ,c_3p::float as c_3_point_field_goals_per_game
+            ,c_3pa::float as  c_3_point_field_goal_attempts_per_game
+            ,c_3p_pct::float as c_3_point_field_goal_percentage
+            ,c_2p::float as c_2_point_field_goals_per_game
+            ,c_2pa::float as c_2_point_field_goal_attempts_per_game
+            ,c_2p_pct::float as c_2_point_field_goal_percentage
+            ,efg_pct::float as effective_field_goal_percentage
+            ,ft::float as free_throws_per_game
+            ,fta::float as  free_throw_attempts_per_game
+            ,ft_pct::float as free_throw_percentage
+            ,orb::float as  offensive_rebounds_per_game
+            ,drb::float as defensive_rebounds_per_game
+            ,trb::float as total_rebounds_per_game
+            ,ast::float as assists_per_game
+            ,stl::float as steals_per_game
+            ,blk::float as blocks_per_game
+            ,tov::float as turnovers_per_game
+            ,pf::float as personal_fouls_per_game
+            ,pts::float as points_per_game
+            ,year::varchar(1000) as season_id
         
         from nba_player_stats
 )
--- models/staging/stg_nba_playoff_stats__player/stg_nba_playoff_stats__player.sql
+++ models/staging/stg_nba_playoff_stats__player/stg_nba_playoff_stats__player.sql
@@ -2,60 +2,58 @@ with nba_playoff_stats__players as (
     select * from {{ ref('nba_playoff_stats')}}
 )
 
-
-
 , renaming_playoff_players_columns as  (
-          select season 
-                ,replace(player, '-', ' ') as player_full_name
-                ,pos as position
-                ,age 
-                ,team_id as team 
-                ,g as playoff_games_in_season
-                ,gs as playoff_games_started_in_season
-                ,mp_per_g as minutes_played_per_game
-                ,fg_per_g as field_goals_per_game
-                ,fga_per_g as field_goal_attempts_per_game
-                ,fg_pct as field_goal_percentage
-                ,fg3_per_g as a_3_point_field_goals_per_game
-                ,fg3a_per_g as a_3_point_field_goal_attempts_per_game
-                ,fg3_pct as a_3_point_field_goal_percentage
-                ,fg2_per_g as a_2_point_field_goals_per_game
-                ,fg2a_per_g as a_2_point_field_goal_attempts_per_game
-                ,fg2_pct as a_2_point_field_goal_percentage
-                ,efg_pct as effective_field_goal_percentage
-                ,ft_per_g as free_throws_per_game
-                ,fta_per_g as free_throw_attempts_per_game
-                ,ft_pct as free_throw_percentage
-                ,orb_per_g as offensive_rebounds_per_game
-                ,drb_per_g as defensive_rebounds_per_game
-                ,trb_per_g as total_rebounds_per_game
-                ,ast_per_g as assists_per_game
-                ,stl_per_g as steals_per_game
-                ,blk_per_g as blocks_per_game
-                ,tov_per_g as turnovers_per_game
-                ,pf_per_g as  personal_fouls_per_game
-                ,pts_per_g as points
-                ,ast_pct as assist_percentage
-                ,blk_pct as block_percentage
-                ,bpm as box_plus_minus
-                ,dbpm as defensive_box_plus_minus
-                ,drb_pct as defensive_rebounding_percentage
-                ,dws as defensive_win_share
-                ,fg3a_per_fga_pct as a_3_point_shot_attempts_per_field_goal_attempted
-                ,fta_per_fga_pct as free_throw_attempted_per_field_goal_attempted_percentage
-                ,mp as total_minutes_played
-                ,obpm as offensive_box_plus_minus
-                ,orb_pct as offensive_rebounding_percentage
-                ,ows as offensive_win_share
-                ,per as player_efficiency_rating
-                ,stl_pct as steal_percentage
-                ,tov_pct as turnover_percentage
-                ,trb_pct as total_rebound_percentage
-                ,ts_pct as true_shooting_percentage
-                ,usg_pct as usage_percentage
-                ,vorp as value_over_replacement_player
-                ,ws as win_share
-                ,ws_per_48 as  win_share_per_48_games
+          select {{ get_season('season') }} AS season_id
+                ,replace(player, '-', ' ')::varchar(1000) as player_full_name
+                ,pos::varchar(1000) as position
+                ,age::int 
+                ,team_id::varchar(1000) as team 
+                ,g::int as playoff_games_in_season
+                ,gs::int as playoff_games_started_in_season
+                ,mp_per_g::float as minutes_played_per_game
+                ,fg_per_g::float as field_goals_per_game
+                ,fga_per_g::float as field_goal_attempts_per_game
+                ,fg_pct::float as field_goal_percentage
+                ,fg3_per_g::float as a_3_point_field_goals_per_game
+                ,fg3a_per_g::float as a_3_point_field_goal_attempts_per_game
+                ,fg3_pct::float as a_3_point_field_goal_percentage
+                ,fg2_per_g::float as a_2_point_field_goals_per_game
+                ,fg2a_per_g::float as a_2_point_field_goal_attempts_per_game
+                ,fg2_pct::float as a_2_point_field_goal_percentage
+                ,efg_pct::float as effective_field_goal_percentage
+                ,ft_per_g::float as free_throws_per_game
+                ,fta_per_g::float as free_throw_attempts_per_game
+                ,ft_pct::float as free_throw_percentage
+                ,orb_per_g::float as offensive_rebounds_per_game
+                ,drb_per_g::float as defensive_rebounds_per_game
+                ,trb_per_g::float as total_rebounds_per_game
+                ,ast_per_g::float as assists_per_game
+                ,stl_per_g::float as steals_per_game
+                ,blk_per_g::float as blocks_per_game
+                ,tov_per_g::float as turnovers_per_game
+                ,pf_per_g::float as  personal_fouls_per_game
+                ,pts_per_g::float as points
+                ,ast_pct::float as assist_percentage
+                ,blk_pct::float as block_percentage
+                ,bpm::float as box_plus_minus
+                ,dbpm::float as defensive_box_plus_minus
+                ,drb_pct::float as defensive_rebounding_percentage
+                ,dws::float as defensive_win_share
+                ,fg3a_per_fga_pct::float as a_3_point_shot_attempts_per_field_goal_attempted
+                ,fta_per_fga_pct::float as free_throw_attempted_per_field_goal_attempted_percentage
+                ,mp::int as total_minutes_played
+                ,obpm::float as offensive_box_plus_minus
+                ,orb_pct::float as offensive_rebounding_percentage
+                ,ows::float as offensive_win_share
+                ,per::float as player_efficiency_rating
+                ,stl_pct::float as steal_percentage
+                ,tov_pct::float as turnover_percentage
+                ,trb_pct::float as total_rebound_percentage
+                ,ts_pct::float as true_shooting_percentage
+                ,usg_pct::float as usage_percentage
+                ,vorp::float as value_over_replacement_player
+                ,ws::float as win_share
+                ,ws_per_48::float as  win_share_per_48_games
 
             from nba_playoff_stats__players
 )
