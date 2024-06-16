1b70a8a681cb25dd049f77173a6393a74f7d350c
Adetayo Aruwajoye <adetayoajilo@gmail.com>
2024-06-16 01:20:27 +0000

    modify the season column on the mvp tables

--- models/mart/fct__regular_rookie_playoffs_mvps_full_players_stats_seasons_97_22.sql
+++ models/mart/fct__regular_rookie_playoffs_mvps_full_players_stats_seasons_97_22.sql
@@ -7,21 +7,47 @@ with playoffs as (
     select *    
      from {{ ref('int_nba_player_stats__players_97_22_with_added_team_name_column') }}
 )
-,
+,rookies as (
+    select *    
+     from {{ ref('stg_nba_player_stats__nba_total_rookies_stats_1980_2023') }}
+)
+
+,regular_season_mvp as (
+    select *    
+     from {{ ref('stg_nba_player_stats__regular_season_mvp_player_stats') }}
+)
+
+,finals_mvp as (
+    select *    
+     from {{ ref('stg_nba_player_stats__playoffs_mvp_finals') }}
+)
 ,regular_plus_playoffs as (
 
     select
-        --   count(1) as num_records
-        --   {{ dbt_utils.generate_surrogate_key(['r.seasons']) }} as dim_season_key
-        --   ,{{ dbt_utils.generate_surrogate_key(['r.player_id', 'r.player_name', 'r.year_of_birth']) }} as dim_player_key
-        --   ,{{ dbt_utils.generate_surrogate_key(['r.teams', 'r.team_name']) }} as dim_team_key
-          r.player_id
+        --   count(1) as num_records --13177
+          {{ dbt_utils.generate_surrogate_key(['r.seasons']) }} as dim_season_key
+          ,{{ dbt_utils.generate_surrogate_key(['r.player_id', 'r.player_name', 'r.year_of_birth']) }} as dim_player_key
+          ,{{ dbt_utils.generate_surrogate_key(['r.teams', 'r.team_name']) }} as dim_team_key
+          ,r.player_id
          ,p.player_id
+         ,ro.player_id
+         ,rsm.player_id
+         ,fm.player_id
+        
       from regular r 
       left join playoffs p  on r.player_id = p.player_id 
                             and r.teams = p.teams 
                             and r.seasons = p.seasons
-                            
+      left join rookies ro  on r.player_id = ro.player_id 
+                            and r.teams = ro.teams 
+                            and r.seasons = ro.seasons
+      left join regular_season_mvp rsm  on r.player_id = rsm.player_id 
+                                        and r.teams = rsm.teams 
+                                        and r.seasons = rsm.seasons
+      left join finals_mvp fm  on r.player_id = fm.player_id 
+                                        and r.teams = fm.teams 
+                                        and r.seasons = fm.seasons
+      
     --   order by r.seasons
 )
 
