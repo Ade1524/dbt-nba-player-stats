ebf8be80e4251696d8eb92b0a56160660588534c
Adetayo Aruwajoye <adetayoajilo@gmail.com>
2024-06-18 08:22:51 +0000

    Rename the dim table and the key columns

--- models/mart/dim_nba_players.sql
+++ models/mart/dim_nba_players.sql
@@ -3,7 +3,7 @@ with table_players as (
      from {{ ref('int_nba_player_stats__all_players_season_97_22_and_rookies_and_playoffs') }}
 )
 , players as (
-    select {{ dbt_utils.generate_surrogate_key(['player_id', 'player_name', 'year_of_birth']) }} as player_key
+    select {{ dbt_utils.generate_surrogate_key(['player_id', 'player_name', 'year_of_birth']) }} as dim_player_key
           ,player_id
           ,player_name
           ,year_of_birth
