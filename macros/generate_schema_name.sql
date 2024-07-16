3b8ff064098d8e3a880921f6ecb4e6d7606770ef
Adetayo Aruwajoye <adetayoajilo@gmail.com>
2024-07-16 10:07:14 +0000

    Refactored the generate_schema_name macro

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,17 +1,18 @@
 /*
 Custom schema logic
 */
-
 {% macro generate_schema_name(custom_schema_name, node) -%}
-    
-    {%- set default_schema = target.schema -%}
 
-    {%- if custom_schema_name is none -%}        
-        {{ default_schema }}
-    {%- elif default_schema == "default" -%}
+    {%- set target_schema_name = target.schema -%}
+    {{ log('custom_schema_name=' ~ custom_schema_name, info=True) }}
+    {{ log('target_schema_name=' ~ target_schema_name, info=True) }}
+
+    {%- if custom_schema_name is none -%}
+        {{ target_schema_name | trim }}
+    {%- elif target_schema_name | lower in ('default', 'none', 'prod') -%}
         {{ custom_schema_name | trim }}
     {%- else -%}
-        {{ default_schema }}_{{ custom_schema_name | trim }}
+        {{ target_schema_name }}_{{ custom_schema_name | trim }}
     {%- endif -%}
 
 {%- endmacro %}
